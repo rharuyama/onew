91de59ab70d7c0eb5b0e8f33acaeeeb02ca02963
Ryo Haruyama <ryoh@Ryos-MacBook-Air.local>
2022-04-24 18:53:36 +0900

    update onew.sh

--- onew.sh
+++ onew.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+if [ $# -eq 0 ]
+  then
+    echo "Usage:\n    onew [NAME]"
+fi
+
 cp -R /Users/ryoh/.onew/template/ $(pwd)/$1/
 mv $(pwd)/$1/template.org $(pwd)/$1/$1.org
 perl -pi -e "s/template/$1/" $(pwd)/$1/Makefile
