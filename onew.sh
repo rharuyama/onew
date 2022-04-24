a8bf54e0d5c8782b3a3d60d48091d0d6c896dfb9
Ryo Haruyama <ryoh@Ryos-MacBook-Air.local>
2022-04-24 18:44:13 +0900

    update onew.sh

--- onew.sh
+++ onew.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-cp -R /Users/ryoh/.org-template/template/ $(pwd)/$1/
+cp -R /Users/ryoh/.onew/template/ $(pwd)/$1/
 mv $(pwd)/$1/template.org $(pwd)/$1/$1.org
+perl -pi -e "s/template/$1/" $(pwd)/$1/Makefile
